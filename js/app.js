b09e6f8917935ba0d2d0042c8241034225423924
angelo friello <angelofriello01@gmail.com>
2023-11-02 16:01:38 +0100

    add bonus

--- js/app.js
+++ js/app.js
@@ -7,18 +7,17 @@ createApp({
 		}
 	},
 	methods: {
-		fetchEmail(n) {
+        fetchEmail(n) {
             for(let i = 0; i < n; i++){
                 axios
 				.get('https://flynn.boolean.careers/exercises/api/random/mail')
 				.then((res) => {
-					console.log(res.data)
+					console.log(res.data, this.emails.length)
                     const email = res.data.response
                     this.emails.push(email)
-                })
-                
+					
+                })           
             }
-			
 		},
 	},
 	created() {
